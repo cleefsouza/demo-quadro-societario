d94aa17c0735953bbbb8a8d96ba3c83de293219a
cleefsouza <a.cleef.souza@gmail.com>
2020-02-08 15:19:26 -0300

    Services

---
+++ api-rest/src/Services/EmpresaService.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\Entity\Empresa;
+use App\Repository\EmpresaRepository;
+
+class EmpresaService {
+
+    /**
+     * @var EmpresaRepository
+     */
+    private $repository;
+
+    public function __construct (EmpresaRepository $repository) {
+        $this->repository = $repository;
+    }
+
+    public function criarEmpresa(string $json) : Empresa {
+
+        $dataJson = json_decode($json);
+
+        $empresa = new Empresa();
+        $empresa
+            ->setRazaoSocial($dataJson->razaoSocial)
+            ->setNomeFantasia($dataJson->nomeFantasia)
+            ->setAtividadePrincipal($dataJson->atividadePrincipal)
+            ->setCnpj($dataJson->cnpj)
+            ->setSituacaoCadastral($dataJson->situacaoCadastral)
+            ->setDataAbertura(new \DateTime($dataJson->dataAbertura));
+
+        return $empresa;
+    }
+
+    public function atualizarEmpresa(int $id, Empresa $empresaAux) : Empresa {
+
+        $empresa = $this->repository->find($id);
+
+        if (!is_null($empresa)) {
+            $empresa
+                ->setRazaoSocial($empresaAux->getRazaoSocial())
+                ->setNomeFantasia($empresaAux->getNomeFantasia())
+                ->setAtividadePrincipal($empresaAux->getAtividadePrincipal())
+                ->setCnpj($empresaAux->getCnpj())
+                ->setSituacaoCadastral($empresaAux->getSituacaoCadastral())
+                ->setDataAbertura($empresaAux->getDataAbertura());
+        };
+
+        return $empresa;
+    }
+}
---
+++ api-rest/src/Services/SocioService.php
@@ -0,0 +1,66 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\Entity\Socio;
+use App\Entity\Empresa;
+use App\Repository\EmpresaRepository;
+use App\Repository\SocioRepository;
+
+class SocioService {
+
+    /**
+     * @var SocioRepository
+     */
+    private $repository;
+    /**
+     * @var EmpresaRepository
+     */
+    private $empresaRepository;
+
+    public function __construct (
+        SocioRepository $repository,
+        EmpresaRepository $empresaRepository
+    ) {
+        $this->repository = $repository;
+        $this->empresaRepository = $empresaRepository;
+    }
+
+    public function criarSocio(string $json) : Socio {
+
+        $dataJson = json_decode($json);
+        $empresaId = $dataJson->empresaId;
+        $empresa = $this->empresaRepository->find($empresaId);
+
+        $socio = new Socio();
+        $socio
+            ->setNomeCompleto($dataJson->nomeCompleto)
+            ->setCpf($dataJson->cpf)
+            ->setEmail($dataJson->email)
+            ->setNascimento(new \DateTime($dataJson->nascimento))
+            ->setSexo($dataJson->sexo)
+            ->setEmpresa($empresa);
+
+        return $socio;
+    }
+
+    public function atualizarSocio(int $id, Socio $socioAux) : Socio {
+
+        $socio = $this->repository->find($id);
+
+        if (!is_null($socio)) {
+            $socio
+                ->setNomeCompleto($socioAux->getNomeCompleto())
+                ->setCpf($socioAux->getCpf())
+                ->setEmail($socioAux->getEmail())
+                ->setSexo($socioAux->getSexo())
+                ->setNascimento($socioAux->getNascimento())
+                ->setEmpresa($socioAux->getEmpresa());
+        };
+
+        return $socio;
+    }
+
+}
